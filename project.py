2f04549c60542004d55c23dbe9b026a284660d8f
xDhairyax <66120130+xDhairyax@users.noreply.github.com>
2021-12-29 19:48:17 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,47 @@
+from flask import Flask,jsonify, request
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Contacts',
+        'description': 'List of all contacts', 
+        'done': False
+    },
+]
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/add-data", methods=["POST"])
+def add_task():
+    if not request.json:
+        return jsonify({
+            "status":"error",
+            "message": "Please provide the data!"
+        },400)
+
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({
+        "status":"success",
+        "message": "Task added succesfully!"
+    })
+    
+
+@app.route("/get-data")
+def get_task():
+    return jsonify({
+        "data" : tasks
+    }) 
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
+
